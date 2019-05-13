a0eb7d5a504a585a117e6fba9da3c53541af7c13
Efdal İncesu <efdal_incesu@hotmail.com>
2019-05-13 17:10:08 +0300

    package

--- helpers/Sequelize.js
+++ helpers/Sequelize.js
@@ -1,7 +1,7 @@
 Sequelize=require("sequelize");
 
-const sequelize=new Sequelize("myblog","root","",{
-    host:"localhost",
+const sequelize=new Sequelize("sql7291620","sql7291620","luXXTu5yn2",{
+    host:"sql7.freesqldatabase.com",
     dialect:"mysql"
 });
 
